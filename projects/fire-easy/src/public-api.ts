ef41860eb7c7789c61c7bdb22476e830acbe352c
Praven <praven2010@live.com>
2023-03-17 22:54:20 -0500

    Added services to the public api

--- projects/fire-easy/src/public-api.ts
+++ projects/fire-easy/src/public-api.ts
@@ -6,3 +6,5 @@ export * from './lib/model';
 export * from './lib/interface';
 export * from './lib/collection.service';
 export * from './lib/pagination.service';
+export * from './lib/stream.service';
+export * from './lib/document.service';
